faa65c538bdf4b787244a09c5eb96f547d6ad6ea
unknown <zach-morris@lambdastudents.com>
2021-09-10 13:50:20 -0400

    Resources/Tasks/Projects running

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,20 @@
-// build your `Project` model here
+//Imports
+const db = require('../../data/dbConfig')
+
+
+//Helper-Functions
+function getProject() {
+    return db('projects')
+}
+
+async function createProject(project) {
+    const [project_id] = await db('projects').insert(project)
+    return getProject().where({ project_id }).first()
+}
+
+
+//Exports; Exposing
+module.exports = {
+    getProject,
+    createProject,
+}
--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,27 @@
-// build your `Task` model here
+//Imports
+const db = require('../../data/dbConfig')
+
+
+//Helper-Functions
+function getTask() {
+    return db('tasks as t')
+        .leftJoin('projects as p', 'p.project_id', 't.project_id')
+        .select('t.task_id', 
+                't.task_description', 
+                't.task_notes', 
+                't.task_completed', 
+                'p.project_name', 
+                'p.project_description')
+}
+
+async function createTask(task) {
+    const [task_id] = await db('tasks').insert(task)
+    return getTask().where({ task_id }).first()
+}
+
+
+//Exports; Exposing
+module.exports = {
+    getTask,
+    createTask,
+}
